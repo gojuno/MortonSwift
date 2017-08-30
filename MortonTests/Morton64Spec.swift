ec603ec8eb8d19815856a6524593659d00b8b5e2
Dmitry Volk <dvolk@gojuno.com>
2017-08-30 20:53:02 +0300

    Migrate to swift 3.0

--- MortonTests/Morton64Spec.swift
+++ MortonTests/Morton64Spec.swift
@@ -9,7 +9,7 @@ final class Morton64Spec: QuickSpec {
 
     override func spec() {
 
-        func doTestInitialize(dimensions: UInt64, _ bits: UInt64) {
+        func doTestInitialize(_ dimensions: UInt64, _ bits: UInt64) {
             it("can't initialize with dimensions \(dimensions) and bits \(bits)") {
                 expect {
                     try Morton64(dimensions: dimensions, bits: bits)
@@ -17,27 +17,27 @@ final class Morton64Spec: QuickSpec {
             }
         }
 
-        func doTestValueBoundaries(dimensions: UInt64, _ bits: UInt64, _ value: UInt64) {
+        func doTestValueBoundaries(_ dimensions: UInt64, _ bits: UInt64, _ value: UInt64) {
             it("can't pack out of boundaries value") {
                 let m: Morton64 = try! Morton64(dimensions: dimensions, bits: bits)
-                let values = [UInt64](count: Int(dimensions), repeatedValue: value)
+                let values = [UInt64](repeating: value, count: Int(dimensions))
                 expect {
                     try m.pack(values)
                 }.to(throwError())
             }
         }
 
-        func doTestSValueBoundaries(dimensions: UInt64, _ bits: UInt64, _ value: Int64) {
+        func doTestSValueBoundaries(_ dimensions: UInt64, _ bits: UInt64, _ value: Int64) {
             it("can't pack out of boundaries signed value") {
                 let m: Morton64 = try! Morton64(dimensions: dimensions, bits: bits)
-                let values = [Int64](count: Int(dimensions), repeatedValue: value)
+                let values = [Int64](repeating: value, count: Int(dimensions))
                 expect {
                     try m.sPack(values)
                 }.to(throwError())
             }
         }
 
-        func doTestPackUnpack(dimensions: UInt64, _ bits: UInt64, _ values: [UInt64]) {
+        func doTestPackUnpack(_ dimensions: UInt64, _ bits: UInt64, _ values: [UInt64]) {
             it("packed/unpacked \(values) with \(dimensions) dimensions and \(bits) bits") {
                 let m: Morton64 = try! Morton64(dimensions: dimensions, bits: bits)
                 let code: Int64 = try! m.pack(values)
@@ -46,7 +46,7 @@ final class Morton64Spec: QuickSpec {
             }
         }
 
-        func doTestSPackUnpack(dimensions: UInt64, _ bits: UInt64, _ values: [Int64]) {
+        func doTestSPackUnpack(_ dimensions: UInt64, _ bits: UInt64, _ values: [Int64]) {
             it("signed packed/unpacked \(values) with \(dimensions) dimensions and \(bits) bits") {
                 let m: Morton64 = try! Morton64(dimensions: dimensions, bits: bits)
                 let code: Int64 = try! m.sPack(values)
@@ -76,59 +76,52 @@ final class Morton64Spec: QuickSpec {
         describe("pack/unpack") {
             doTestPackUnpack(2, 32, [1, 2])
             doTestPackUnpack(2, 32, [2, 1])
-            doTestPackUnpack(2, 32, [(1 << 32) - 1, (1 << 32) - 1])
+            doTestPackUnpack(2, 32, [UInt64](repeating: UInt64((1 << 32) - 1), count: 2))
             doTestPackUnpack(2, 1, [1, 1])
 
             doTestPackUnpack(3, 21, [1, 2, 4])
             doTestPackUnpack(3, 21, [4, 2, 1])
-            let values0: [UInt64] = [(1 << 21) - 1, (1 << 21) - 1, (1 << 21) - 1]
-            doTestPackUnpack(3, 21, values0)
+            doTestPackUnpack(3, 21, [UInt64](repeating: UInt64((1 << 21) - 1), count: 3))
             doTestPackUnpack(3, 1, [1, 1, 1])
 
             doTestPackUnpack(4, 16, [1, 2, 4, 8])
             doTestPackUnpack(4, 16, [8, 4, 2, 1])
-            let values1: [UInt64] = [(1 << 16) - 1, (1 << 16) - 1, (1 << 16) - 1, (1 << 16) - 1]
-            doTestPackUnpack(4, 16, values1)
+            doTestPackUnpack(4, 16, [UInt64](repeating: UInt64((1 << 16) - 1), count: 4))
             doTestPackUnpack(4, 1, [1, 1, 1, 1])
 
             doTestPackUnpack(6, 10, [1, 2, 4, 8, 16, 32])
             doTestPackUnpack(6, 10, [32, 16, 8, 4, 2, 1])
             doTestPackUnpack(6, 10, [1023, 1023, 1023, 1023, 1023, 1023])
 
-            let values: [UInt64] = [UInt64](count: 64, repeatedValue: 1)
-            doTestPackUnpack(64, 1, values)
+            doTestPackUnpack(64, 1, [UInt64](repeating: 1, count: 64))
         }
 
         describe("spack/sunpack") {
             doTestSPackUnpack(2, 32, [1, 2])
             doTestSPackUnpack(2, 32, [2, 1])
-            doTestSPackUnpack(2, 32, [(1 << 31) - 1, (1 << 31) - 1])
+            doTestSPackUnpack(2, 32, [Int64](repeating: Int64((1 << 31) - 1), count: 2))
             doTestSPackUnpack(2, 2, [1, 1])
             doTestSPackUnpack(2, 32, [-1, -2])
             doTestSPackUnpack(2, 32, [-2, -1])
-            doTestSPackUnpack(2, 32, [-((1 << 31) - 1), -((1 << 31) - 1)])
+            doTestSPackUnpack(2, 32, [Int64](repeating: Int64(-((1 << 31) - 1)), count: 2))
             doTestSPackUnpack(2, 2, [-1, -1])
 
             doTestSPackUnpack(3, 21, [1, 2, 4])
             doTestSPackUnpack(3, 21, [4, 2, 1])
-            let values0: [Int64] = [(1 << 20) - 1, (1 << 20) - 1, (1 << 20) - 1]
-            doTestSPackUnpack(3, 21, values0)
+            doTestSPackUnpack(3, 21, [Int64](repeating: Int64((1 << 20) - 1), count: 3))
             doTestSPackUnpack(3, 2, [1, 1, 1])
             doTestSPackUnpack(3, 21, [-1, -2, -4])
             doTestSPackUnpack(3, 21, [-4, -2, -1])
-            let values1: [Int64] = [-((1 << 20) - 1), -((1 << 20) - 1), -((1 << 20) - 1)]
-            doTestSPackUnpack(3, 21, values1)
+            doTestSPackUnpack(3, 21, [Int64](repeating: Int64(-((1 << 20) - 1)), count: 3))
             doTestSPackUnpack(3, 2, [-1, -1, -1])
 
             doTestSPackUnpack(4, 16, [1, 2, 4, 8])
             doTestSPackUnpack(4, 16, [8, 4, 2, 1])
-            let values2: [Int64] = [(1 << 15) - 1, (1 << 15) - 1, (1 << 15) - 1, (1 << 15) - 1]
-            doTestSPackUnpack(4, 16, values2)
+            doTestSPackUnpack(4, 16, [Int64](repeating: Int64((1 << 15) - 1), count: 4))
             doTestSPackUnpack(4, 2, [1, 1, 1, 1])
             doTestSPackUnpack(4, 16, [-1, -2, -4, -8])
             doTestSPackUnpack(4, 16, [-8, -4, -2, -1])
-            let values3: [Int64] = [-((1 << 15) - 1), -((1 << 15) - 1), -((1 << 15) - 1), -((1 << 15) - 1)]
-            doTestSPackUnpack(4, 16, values3)
+            doTestSPackUnpack(4, 16, [Int64](repeating: Int64(-((1 << 15) - 1)), count: 4))
             doTestSPackUnpack(4, 2, [-1, -1, -1, -1])
 
             doTestSPackUnpack(6, 10, [1, 2, 4, 8, 16, 32])
@@ -138,9 +131,7 @@ final class Morton64Spec: QuickSpec {
             doTestSPackUnpack(6, 10, [-32, -16, -8, -4, -2, -1])
             doTestSPackUnpack(6, 10, [-511, -511, -511, -511, -511, -511])
 
-            let range: Range<Int64> = 0..<32
-            let values = range.map { 1 - 2 * ($0 % 2) }
-            doTestSPackUnpack(32, 2, values)
+            doTestSPackUnpack(32, 2, [Int64](0..<32).map { 1 - 2 * ($0 % 2) })
         }
 
         describe("variadic pack") {
